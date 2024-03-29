6bc2f3676cbfad1c331f5682d5a092be0765acd6
cwb00010 <142524886+cwb00010@users.noreply.github.com>
2024-03-28 23:44:11 -0400

    Update IUserService.cs

--- WillBruton_Code/Repositories/IUserService.cs
+++ WillBruton_Code/Repositories/IUserService.cs
@@ -1,1 +1,14 @@
+using System.Threading.Tasks;
+using FarmMaxSiteAPI.Entities;
 
+
+namespace FarmMaxSiteAPI.Repositiories
+{
+    public interface IUserService
+    {
+        public Task<List<User>> InsertCityID(int CityID);
+        
+        
+
+    }
+}
