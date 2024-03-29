89b624a2b754e2b4a7cc4b84841cad2214a79620
cwb00010 <142524886+cwb00010@users.noreply.github.com>
2024-03-28 23:45:13 -0400

    Update UserService.cs

--- WillBruton_Code/Repositories/UserService.cs
+++ WillBruton_Code/Repositories/UserService.cs
@@ -1,1 +1,23 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using FarmMaxSiteAPI.Data;
+using FarmMaxSiteAPI.Entities;
 
+namespace FarmMaxSiteAPI.Repositiories
+{
+    public class UserDetails : IUserService
+    {
+        private readonly DbContextClass _dbContextClass;
+        public UserDetails(DbContextClass dbContextClass)
+        {
+            _dbContextClass = dbContextClass;
+        }
+        public async Task<List<User>> InsertCityID(int CityID)
+        {
+            var param = new SqlParameter("@CityID", CityID);
+            var UserDetails = await Task.Run(() => _dbContextClass.User.FromSqlRaw("exec spGetUserDetailsByCity @CityID", param).ToListAsync());
+            return UserDetails;
+        }
+    }
+}
+       
