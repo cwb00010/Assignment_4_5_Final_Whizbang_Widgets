dde5a6c2753f85d468868195fdf1ff218d392994
cwb00010 <142524886+cwb00010@users.noreply.github.com>
2024-03-28 23:41:57 -0400

    Update Seed.cs

--- WillBruton_Code/Entities/Seed.cs
+++ WillBruton_Code/Entities/Seed.cs
@@ -1,1 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace FarmMaxSiteAPI.Entities
+{
+    public class Seed
+    {
+        public int SeedID { get; set; }
+
+        [Required]
+        public string SeedName { get; set; }
+
+        [Required]  
+        public string IdealPlantTemp { get; set; }
+
+    }
+}
