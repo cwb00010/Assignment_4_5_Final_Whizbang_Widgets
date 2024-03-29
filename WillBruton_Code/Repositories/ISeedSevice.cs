e235de6e59cf71fe085348344b532b0cbfdea602
cwb00010 <142524886+cwb00010@users.noreply.github.com>
2024-03-28 23:43:24 -0400

    Update ISeedSevice.cs

--- WillBruton_Code/Repositories/ISeedSevice.cs
+++ WillBruton_Code/Repositories/ISeedSevice.cs
@@ -1,1 +1,17 @@
+using System.Threading.Tasks;
+using FarmMaxSiteAPI.Entities;
+
+
+
+namespace FarmMaxSiteAPI.Repositiories
+{
+    public interface ISeedService
+    {
+    
+      
+        public Task<List<Seed>> InsertID(int SeedID);
+        public Task<List<Seed>> InsertName(string SeedName);
+        public Task<List<Seed>> InsertTemp(string IdealPlantTemp);
+    }
+}
 
